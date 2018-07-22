a4c8e59da626abd97c511e359122a1ab4b220c1c
Uche Jude <iamuchejude@gmail.com>
2018-07-22 15:07:38 +0100

    Wrote time Utility

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import chalk from 'chalk';
+import time from './util/time';
 
-chalk();
+class PrettyLog {
+  constructor(message) {
+    return (
+      console.log(chalk.bgWhite(time), message)
+    );
+  }
+
+  static error(message) {
+    return console.log(message);
+  }
+
+  static warn(message) {
+    return console.log(message);
+  }
+
+  static success(message) {
+    return console.log(message);
+  }
+
+  static info(message) {
+    return console.log(message);
+  }
+}
+
+export default PrettyLog;
---
+++ src/util/time.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+
+const TIME = moment().format('h:m:sA');
+
+export default TIME;
