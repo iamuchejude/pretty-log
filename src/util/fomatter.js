fc257d90e8b3b8c3a205f3d280a7becaaf8af967
Uche Jude <iamuchejude@gmail.com>
2018-07-22 15:14:05 +0100

    Started writing formatter

---
+++ src/util/fomatter.js
@@ -0,0 +1,24 @@
+class Format {
+  constructor($string) {
+    this.str = $string;
+    this.formatUrl();
+    this.formatEmail();
+    this.formatPhoneNumber();
+
+    return this.str;
+  }
+
+  formatUrl() {
+    return this.str;
+  }
+
+  formatEmail() {
+    return this.str;
+  }
+
+  formatPhoneNumber() {
+    return this.str;
+  }
+}
+
+export default Format;
